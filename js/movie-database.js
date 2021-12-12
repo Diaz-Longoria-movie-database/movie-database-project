917d39a498d4e72f69edbb35395e2ffc92e8222a
Nicholas <nicholasdiazjr509@gmail.com>
2021-12-12 10:35:17 -0600

    more ideas

--- js/movie-database.js
+++ js/movie-database.js
@@ -1,9 +1,33 @@
-const API_URL = 'https://slash-honorable-moustache.glitch.me';
+const API_URL = 'https://slash-honorable-moustache.glitch.me/movies';
+//
+// getMovies(API_URL);
+// function getMovies(url){
+//     fetch(url).then(res => res.json()).then(data => {
+//         showMovies(data.results);
+//     })
+//
+// }
+//
+//
+// function showMovies(data){
+//
+//     data.forEach(movie =>{
+//         const movieEl = document.createElement('div');
+//         movieEl.classList.add('movie');
+//         movieEl.innerHTML = `
+//         <img src="" alt="image">
+// <div class="movie-info">
+//     <h3>Movie Title</h3>
+//     <span class="green">9.8</span>
+// </div>
+// <div class="overview">
+//     <h3>Overview</h3>
+//     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aspernatur cum eos, fuga neque nulla perspiciatis sunt tempora. Aliquam consequuntur deleniti esse natus possimus quasi repellat rerum veniam veritatis voluptas.
+// </div>`
+//     })
+// }
 
-
-
-
-    const textInput = document.querySelector("#inputPart");
+    const textInput = document.querySelector("#search");
     const textOutput = document.querySelector("#showOutPut");
     const btn = document.querySelector("#submitInput");
 
@@ -21,7 +45,7 @@ function hideLoading(){
     loader.classList.remove("display");
 }
 //dummy url
-var url = "loading"
+var url = "https://slash-honorable-moustache.glitch.me/movies"
 
     function fetchHandler(event){
         displayLoading()
